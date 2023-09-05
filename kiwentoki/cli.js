0921b18c8c674a92d47c2175d939be11579df79f
Jordan Reger <mail@jordanreger.com>
2023-09-05 01:50:18 -0400

    fix bug in cli.js

--- kiwentoki/cli.js
+++ kiwentoki/cli.js
@@ -1,7 +1,7 @@
 const search = Deno.args[0] ?? (() => { throw new Error("No word provided") })();
 
 const english = await fetch("https://jordanreger.com/kiwentoki/english.json")
-.then(res => JSON.parse(res));
+.then(res => res.json());
 
 const keys = Object.keys(english.words).filter((key) => {
   if (typeof english.words[key] === "object") return english.words[key].some(def => def.toLowerCase() === search.toLowerCase());
